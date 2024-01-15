e5ae29dcd4b404f264e2a0e6eb08eba449419fc3
Poxie <poxen12@hotmail.com>
2024-01-15 01:05:04 +0100

    Decreased donation section border radius

--- components/donate/index.tsx
+++ components/donate/index.tsx
@@ -2,7 +2,7 @@ import DonationProgress from "./DonationProgress";
 
 export default function Donate() {
     return(
-        <div className="min-w-donate w-donate rounded-xl border-[1px] border-secondary">
+        <div className="min-w-donate w-donate rounded-lg border-[1px] border-secondary">
             <DonationProgress className="p-5 border-b-[1px] border-b-secondary" />
         </div>
     )
