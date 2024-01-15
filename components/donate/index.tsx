5f129c169602b10947938157dd7d98e0dc54a3e9
Poxie <poxen12@hotmail.com>
2024-01-15 01:14:58 +0100

    Added minimalistic donate input

--- components/donate/index.tsx
+++ components/donate/index.tsx
@@ -1,9 +1,24 @@
+"use client";
+import { useState } from "react";
+import Input from "../input";
 import DonationProgress from "./DonationProgress";
 
 export default function Donate() {
+    const [amount, setAmount] = useState('');
+
     return(
         <div className="min-w-donate w-donate rounded-lg border-[1px] border-secondary">
             <DonationProgress className="p-5 border-b-[1px] border-b-secondary" />
+            <div className="p-5">
+                <span className="block mb-2">
+                    Make a change today.
+                </span>
+                <Input 
+                    placeholder="Amount"
+                    onChange={setAmount}
+                    value={amount}
+                />
+            </div>
         </div>
     )
 }
