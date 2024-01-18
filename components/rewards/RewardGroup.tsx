68c2fee812b966f86cfd2c4506e80071785feb98
Poxie <poxen12@hotmail.com>
2024-01-18 02:02:32 +0100

    Opening donation modal with selected tier's amount

--- components/rewards/RewardGroup.tsx
+++ components/rewards/RewardGroup.tsx
@@ -1,8 +1,15 @@
+"use client";
+import { useModal } from "@/contexts/modal";
 import { RewardGroup } from ".";
 import Button from "../button";
 import RewardItem from "./RewardItem";
+import DonateModal from "@/modals/donate";
 
 export default function RewardTier({ amount, title, rewards }: RewardGroup) {
+    const { setModal } = useModal();
+
+    const openDonateModal = () => setModal(<DonateModal amount={amount} />);
+
     return(
         <li className="@container border-[1px] border-secondary rounded-lg hover:shadow-md transition-shadow overflow-hidden">
             <div className="p-4 flex justify-between items-center border-b-[1px] border-b-secondary">
@@ -10,8 +17,8 @@ export default function RewardTier({ amount, title, rewards }: RewardGroup) {
                     ${amount} • {title}
                 </span>
                 <Button 
-                    onClick={() => {}}
-                    className="py-2 px-3 -my-2 -mx-3 text-sm font-bold rounded-md"
+                    onClick={openDonateModal}
+                    className="py-2 px-3 -my-2 -mx-1 text-sm font-bold rounded-md"
                 >
                     Select tier
                 </Button>
