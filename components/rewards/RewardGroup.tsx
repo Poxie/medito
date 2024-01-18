55c66356ee42db0719e59947c61079d17268e342
Poxie <poxen12@hotmail.com>
2024-01-18 13:15:32 +0100

    Made tier group clickable, replaced select text with checkbox

--- components/rewards/RewardGroup.tsx
+++ components/rewards/RewardGroup.tsx
@@ -19,10 +19,13 @@ export default function RewardTier({ id, amount, title, rewards, index }: Reward
     }
 
     return(
-        <li className={twMerge(
-            "@container border-[1px] border-secondary rounded-lg hover:shadow-md transition-[box-shadow,border-color] overflow-hidden",
-            isActive && "border-c-primary shadow-success hover:shadow-success",
-        )}>
+        <li
+            className={twMerge(
+                "@container border-[1px] border-secondary rounded-lg hover:shadow-md transition-[box-shadow,border-color] overflow-hidden cursor-pointer",
+                isActive && "border-c-primary shadow-success hover:shadow-success",
+            )}
+            onClick={onSelectClick}
+        >
             <div className="p-4 flex justify-between items-start gap-1 border-b-[1px] border-b-secondary">
                 <div className="flex items-center flex-wrap">
                     <span className="block text-lg font-medium mr-2">
@@ -34,13 +37,18 @@ export default function RewardTier({ id, amount, title, rewards, index }: Reward
                         </span>
                     )}
                 </div>
-                <Button 
-                    onClick={onSelectClick}
-                    className="py-2 px-3 -m-1 text-sm font-bold rounded-md whitespace-nowrap"
-                    buttonType={!isActive ? "primary" : "secondary"}
+                <button 
+                    className={twMerge(
+                        "w-7 aspect-square flex justify-center items-center rounded-full border-2 border-secondary transition-[border-color]",
+                        isActive && "border-c-primary",
+                    )}
+                    aria-label={isActive ? "Deselect tier" : "Select tier"}
                 >
-                    {isActive ? 'Deselect tier' : 'Select tier'}
-                </Button>
+                    <span className={twMerge(
+                        "block w-[70%] aspect-square bg-c-primary rounded-full scale-100 transition-transform",
+                        !isActive && "scale-0",
+                    )} />
+                </button>
             </div>
             <ul className="grid @2xl:grid-cols-2 gap-[1px] bg-tertiary">
                 {rewards.map((reward, index) => (
