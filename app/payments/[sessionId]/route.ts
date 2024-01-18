1520a6b629e63d9e2a864611a4d2530a7175c3bb
Poxie <poxen12@hotmail.com>
2024-01-18 22:01:02 +0100

    Added route to get a checkout session

---
+++ app/payments/[sessionId]/route.ts
@@ -0,0 +1,19 @@
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export async function GET(request: Request, { params }: { params: { sessionId: string } }) {
+    try {
+        const session = await stripe.checkout.sessions.retrieve(params.sessionId);
+        return Response.json({
+            displayName: session.customer_details?.name,
+            amount: (session.amount_total || 0) / 100,
+            timestamp: session.created * 1000,
+        });
+    } catch(error: any) {
+        if(error.statusCode === 404) {
+            return new Response('Session not found.', { status: 404 });
+        }
+        return new Response('Internal server error.', { status: 500 });
+    }
+}
