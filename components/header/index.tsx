604d9b46ad5fe7b262eb530a22c9ac82259dd299
Poxie <poxen12@hotmail.com>
2024-01-18 15:28:13 +0100

    Moved campagine info into assets folder

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,23 +1,21 @@
+import campaign from '@/assets/data/campaign/index.json';
 import HeaderButtons from "./HeaderButtons";
 
 export default function Header({ className }: {
     className?: string;
 }) {
+    const { title, description } = campaign;
     return(
         <div className={className}>
             <h1 className="text-3xl lg:text-4xl font-bold">
-                Hope for Tomorrow: Empowering Communities Through Education
+                {title}
             </h1>
             <div className="mt-3 flex flex-col gap-3 lg:text-lg text-secondary">
-                <p>
-                    Join us in making a positive impact on the future! Our fundraiser, "Hope for Tomorrow," aims to empower underprivileged communities through the gift of education. By contributing to this cause, you're not just donating; you're investing in the potential of countless bright minds.
-                </p>
-                <p>
-                    Your support will make a lasting difference in the lives of these children, opening doors to a brighter future filled with possibilities. Join us on this journey to build hope, inspire change, and create a world where every child has the chance to dream big and achieve even bigger.
-                </p>
-                <p>
-                    Make a donation today and be a part of the movement that transforms lives through education. Together, we can build a better tomorrow!
-                </p>
+                {description.map((paragraph, index) => (
+                    <p key={index}>
+                        {paragraph}
+                    </p>
+                ))}
             </div>
             <HeaderButtons />
         </div>
