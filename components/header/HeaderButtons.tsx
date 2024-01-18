5561b12bb6b42ebd9ebcf787d78294c6001f9d53
Poxie <poxen12@hotmail.com>
2024-01-18 15:23:38 +0100

    Created utility function to scroll to section

--- components/header/HeaderButtons.tsx
+++ components/header/HeaderButtons.tsx
@@ -3,26 +3,12 @@ import { useModal } from "@/contexts/modal";
 import Button from "../button";
 import DonateModal from "@/modals/donate";
 import DonateModalButton from "../donate/DonateModalButton";
+import { goToSection } from "@/utils";
 
-const REWARD_SECTION_OFFSET = 24;
-const MOBILE_REWARD_SECTION_OFFSET = REWARD_SECTION_OFFSET - 166;
-const MOBILE_THRESHOLD = 1024;
 export default function HeaderButtons() {
     const { setModal } = useModal();
 
     const openDonateModal = () => setModal(<DonateModal />);
-    const goToRewards = () => {
-        const rewards = document.getElementById('rewards');
-        if(rewards) {
-            const isSmallScreen = window.innerWidth < MOBILE_THRESHOLD;
-            const top = rewards.offsetTop + (isSmallScreen ? MOBILE_REWARD_SECTION_OFFSET : REWARD_SECTION_OFFSET);
-
-            window.scrollTo({
-                behavior: 'smooth',
-                top,
-            })
-        }
-    }
 
     return(
         <div className="mt-6 flex gap-3">
@@ -30,7 +16,7 @@ export default function HeaderButtons() {
                 className="flex-1 lg:flex-[unset]"
             />
             <Button
-                onClick={goToRewards}
+                onClick={() => goToSection('rewards')}
                 buttonType="secondary"
                 className="flex-1 lg:flex-[unset]"
             >
