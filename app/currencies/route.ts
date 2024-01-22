cd6e75edaf384687d6c441b0106d0dec0b514dd4
Poxie <poxen12@hotmail.com>
2024-01-22 03:04:32 +0100

    Added api endpoint to get currency exchange rates

---
+++ app/currencies/route.ts
@@ -0,0 +1,16 @@
+import { isValidCurrency } from "@/utils";
+
+export async function GET(request: Request) {
+    const res = await fetch(`${process.env.CURRENCY_API_URL}/latest?api_key=${process.env.CURRENCY_API_KEY}&base=USD`)
+    const data = await res.json();
+    
+    const rates = data.response.rates;
+    
+    for(const currency of Object.keys(rates)) {
+        if(!isValidCurrency(currency)) {
+            delete rates[currency];
+        }
+    }
+
+    return Response.json(rates);
+}
--- environment.d.ts
+++ environment.d.ts
@@ -4,8 +4,10 @@ declare global {
             STRIPE_SECRET_KEY: string;
             STRIPE_PRODUCT_ID: string;
             STRIPE_SUCCESS_URL: string;
-            STRIPE_CANCEL_URL: string;     
-            CF_PAGES_URL: string | undefined;   
+            STRIPE_CANCEL_URL: string;
+            CF_PAGES_URL: string | undefined;
+            CURRENCY_API_URL: string;
+            CURRENCY_API_KEY: string;
         }
     }
 }
