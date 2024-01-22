41b675b3fb3d36b08eecf419c73689a56e927f55
Poxie <poxen12@hotmail.com>
2024-01-22 03:24:51 +0100

    Added focus on mount prop

--- components/input/index.tsx
+++ components/input/index.tsx
@@ -1,11 +1,12 @@
 import { twMerge } from "tailwind-merge";
 
-export default function Input({ type='text', value, onChange, placeholder, className }: {
+export default function Input({ type='text', value, onChange, placeholder, focusOnMount, className }: {
     type?: 'text' | 'textarea';
     value: string;
     onChange: (value: string) => void;
     placeholder?: string;
     className?: string;
+    focusOnMount?: boolean;
 }) {
     const props = { 
         onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => onChange(e.target.value), 
@@ -14,7 +15,8 @@ export default function Input({ type='text', value, onChange, placeholder, class
             className,
         ),
         value, 
-        placeholder, 
+        placeholder,
+        autoFocus: focusOnMount,
     };
     
     return type === 'text' ? <input {...props} /> : <textarea {...props} style={{ minHeight: 100 }} />
